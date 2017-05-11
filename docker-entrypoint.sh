49d419d0c81afc30950628db207348fc0b8d6ac3
Marc Riegel <mail@mrcrgl.de>
2017-05-11 19:56:40 +0200

    Added graceful shutdown period of 5 seconds

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -ef -o pipefail
+
 KEYS="UPSTREAM_SERVER UPSTREAM_MAX_FAILURES CACHE_MAX_ITEMS CACHE_MIN_TTL CACHE_MAX_TTL NETWORK_UDP_PORTS NETWORK_LISTEN WEBSERVICE_ENABLED WEBSERVICE_LISTEN"
 
 if [ -z "${UPSTREAM_SERVER}" ]; then
@@ -45,4 +47,36 @@ do
   sed -i "s|__${name}__|${value}|g" /home/dns/edgedns.toml
 done
 
-$@
+pid=0
+
+# SIGTERM-handler
+term_handler() {
+  echo "[Terminate] Initiate graceful shutdown"
+
+  # Grace period
+  echo "[Terminate] Wait grace period of 5 seconds"
+  sleep 5;
+
+  if [ $pid -ne 0 ]; then
+    echo "[Terminate] Terminate EdgeDNS"
+    kill -SIGTERM "$pid" || :
+    wait "$pid" || :
+  fi
+
+  exit 143; # 128 + 15 => SIGTERM
+}
+
+# setup handlers
+# on callback, kill the last background process, which is `tail -f /dev/null` and execute the specified handler
+trap 'kill ${!}; term_handler' SIGTERM
+trap 'kill ${!}; term_handler' SIGINT
+
+$@ &
+
+pid="$!"
+
+# wait indefinetely
+while true
+do
+  tail -f /dev/null & wait ${!}
+done
